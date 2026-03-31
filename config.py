90c3af054b05a7a9b04c04f815795731082b5f2a
Moysés Voss <moyseshvoss@gmail.com>
2026-03-31 07:42:01 -0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-muito-segura'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'nucleotech.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
